b8897a72888694b354d6593a7fdf849acacd8f65
LYJ <lyj.blessed2@gmail.com>
2024-12-26 10:40:37 +0900

    feat : 1. 트랜잭션 이해 수정

--- src/main/java/com/example/demo/service/ReservationService.java
+++ src/main/java/com/example/demo/service/ReservationService.java
@@ -35,6 +35,7 @@ public class ReservationService {
     }
 
     // TODO: 1. 트랜잭션 이해
+    @Transactional
     public void createReservation(Long itemId, Long userId, LocalDateTime startAt, LocalDateTime endAt) {
         // 쉽게 데이터를 생성하려면 아래 유효성검사 주석 처리
         List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
