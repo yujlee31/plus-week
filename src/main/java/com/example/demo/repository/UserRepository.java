ec421285f28d46e822e1f6185709cbcf262ba924
LYJ <lyj.blessed2@gmail.com>
2024-12-26 14:08:52 +0900

    feat : 4.find,save 반복 개선 수정

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.status = 'PENDING' WHERE u.status = 'APPROVED' AND u.role = 'USER' AND u.id IN :userIds")
+    void updatePendingStatus(List<Long> userIds);
 }
--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -18,12 +18,19 @@ public class AdminService {
     // TODO: 4. find or save 예제 개선
     @Transactional
     public void reportUsers(List<Long> userIds) {
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+//        for (Long userId : userIds) {
+//            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+//
+//            user.updateStatusToBlocked();
+//
+//            userRepository.save(user);
+//        }
+        List<User> users = userRepository.findAllById(userIds); //List를 파라미터로 받는 메서드로 변경
 
-            user.updateStatusToBlocked();
-
-            userRepository.save(user);
+        if(users.isEmpty()) {
+            throw new IllegalArgumentException("해당 아이디에 대한 유저 권한인 사용자가 존재하지 않습니다.");
         }
+
+        userRepository.updatePendingStatus(userIds);
     }
 }
