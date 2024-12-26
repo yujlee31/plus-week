cd07c9f90154e50856aab3c74d7cf8dfc1d9a3ec
LYJ <lyj.blessed2@gmail.com>
2024-12-26 15:45:48 +0900

    feat : 7.리팩토링(자주쓰이는 findby itemId default함수로) 수정

--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    default Item findItemById(Long id) {
+        return findById(id).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+    }
 }
