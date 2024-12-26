5a43a9ad50ffd69a51f220961dcc2887aad2e154
LYJ <lyj.blessed2@gmail.com>
2024-12-26 15:47:22 +0900

    feat : 7.리팩토링(컨트롤러 반환타입) 수정

--- src/main/java/com/example/demo/service/ReservationService.java
+++ src/main/java/com/example/demo/service/ReservationService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.ReservationResponseDto;
+import com.example.demo.dto.ReservationUpdateResponseDto;
 import com.example.demo.entity.Item;
 import com.example.demo.entity.RentalLog;
 import com.example.demo.entity.Reservation;
@@ -36,20 +37,29 @@ public class ReservationService {
 
     // TODO: 1. 트랜잭션 이해
     @Transactional
-    public void createReservation(Long itemId, Long userId, LocalDateTime startAt, LocalDateTime endAt) {
+    public ReservationResponseDto createReservation(Long itemId, Long userId, LocalDateTime startAt, LocalDateTime endAt) {
         // 쉽게 데이터를 생성하려면 아래 유효성검사 주석 처리
         List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
         if(!haveReservations.isEmpty()) {
             throw new ReservationConflictException("해당 물건은 이미 그 시간에 예약이 있습니다.");
         }
 
-        Item item = itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+        //Item item = itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+        Item item = itemRepository.findItemById(itemId);
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
         Reservation reservation = new Reservation(item, user, "PENDING", startAt, endAt);
         Reservation savedReservation = reservationRepository.save(reservation);
 
         RentalLog rentalLog = new RentalLog(savedReservation, "로그 메세지", "CREATE");
         rentalLogService.save(rentalLog);
+
+        return new ReservationResponseDto(
+                savedReservation.getId(),
+                savedReservation.getUser().getNickname(),
+                savedReservation.getItem().getName(),
+                savedReservation.getStartAt(),
+                savedReservation.getEndAt()
+        );
     }
 
     // TODO: 3. N+1 문제
@@ -102,26 +112,26 @@ public class ReservationService {
 
     // TODO: 7. 리팩토링
     @Transactional
-    public void updateReservationStatus(Long reservationId, String status) {
+    public ReservationUpdateResponseDto updateReservationStatus(Long reservationId, String status) {
         Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 데이터가 존재하지 않습니다."));
 
-        if ("APPROVED".equals(status)) {
-            if (!"PENDING".equals(reservation.getStatus())) {
+        if ("APPROVED".equals(status) && !"PENDING".equals(reservation.getStatus())) {
                 throw new IllegalArgumentException("PENDING 상태만 APPROVED로 변경 가능합니다.");
-            }
-            reservation.updateStatus("APPROVED");
-        } else if ("CANCELED".equals(status)) {
-            if ("EXPIRED".equals(reservation.getStatus())) {
-                throw new IllegalArgumentException("EXPIRED 상태인 예약은 취소할 수 없습니다.");
-            }
-            reservation.updateStatus("CANCELED");
-        } else if ("EXPIRED".equals(status)) {
-            if (!"PENDING".equals(reservation.getStatus())) {
-                throw new IllegalArgumentException("PENDING 상태만 EXPIRED로 변경 가능합니다.");
-            }
-            reservation.updateStatus("EXPIRED");
-        } else {
-            throw new IllegalArgumentException("올바르지 않은 상태: " + status);
+
         }
+
+        if ("CANCELED".equals(status) && "EXPIRED".equals(reservation.getStatus())) {
+                throw new IllegalArgumentException("EXPIRED 상태인 예약은 취소할 수 없습니다.");
+        }
+
+        if ("EXPIRED".equals(status) && !"PENDING".equals(reservation.getStatus())) {
+                throw new IllegalArgumentException("PENDING 상태만 EXPIRED로 변경 가능합니다.");
+        }
+
+        reservation.updateStatus(status);
+
+        return new ReservationUpdateResponseDto(
+                reservation.getStatus()
+        );
     }
 }
